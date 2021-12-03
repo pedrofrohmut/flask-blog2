bf74bf4cb363398731c690b710f12cc3af7a5e7f
Pedro Frohmut <pedrofrohmut@gmail.com>
2021-12-03 09:39:42 -0300

    User account & profile picture

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,5 +1,7 @@
 """Forms."""
+from flask_login import current_user
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, EmailField
 from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
 
@@ -9,13 +11,10 @@ from flaskblog.models import User
 class SignUpForm(FlaskForm):
     """Generates a form for sign up page."""
 
-    username = StringField("Username",
-                           validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
     email = EmailField("E-mail", validators=[DataRequired(), Email()])
-    password = PasswordField("Password",
-                             validators=[DataRequired(), Length(min=2, max=20)])
-    confirm_password = PasswordField("Confirm password",
-                                     validators=[DataRequired(), EqualTo("password")])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=2, max=20)])
+    confirm_password = PasswordField("Confirm password", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Submit")
 
     def validate_username(self, username):
@@ -35,7 +34,31 @@ class SignInForm(FlaskForm):
     """Generates a form for sign in page."""
 
     email = StringField("E-mail", validators=[DataRequired(), Email()])
-    password = PasswordField("Password",
-                             validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=2, max=20)])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Submit")
+
+
+class UpdateAccountForm(FlaskForm):
+    """Generates a form for account page."""
+
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
+    email = EmailField("E-mail", validators=[DataRequired(), Email()])
+    image = FileField("Update Profile Image", validators=[FileAllowed(["jpg", "png"])])
+    submit = SubmitField("Submit")
+
+    def validate_username(self, username):
+        """Validate if the username is already taken checking the database."""
+        if username.data == current_user.username:
+            return
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Username already taken")
+
+    def validate_email(self, email):
+        """Validate if the e-mail is already taken checking the database."""
+        if email.data == current_user.email:
+            return
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("E-mail already taken")
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,12 @@
 """Application routes."""
+import secrets
+import os
+
+from PIL import Image
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, current_user, logout_user, login_required
 
-from flaskblog.forms import SignUpForm, SignInForm
+from flaskblog.forms import SignUpForm, SignInForm, UpdateAccountForm
 from flaskblog.models import User
 from flaskblog import app, db, bcrypt
 
@@ -61,9 +65,11 @@ def signin():
         user = User.query.filter_by(email=form.email.data).first()
         if not user:
             flash(f"No user found with e-mail: {form.email.data}."
-                  + "Please retry or go to sign up if you are not yet registered", category="error")
+                  + "Please retry or go to sign up if you are not yet registered",
+                  category="error")
             return redirect(url_for("signin"))
-        is_password_valid = bcrypt.check_password_hash(user.password_hash, form.password.data)
+        is_password_valid = bcrypt.check_password_hash(user.password_hash,
+                                                       form.password.data)
         if not is_password_valid:
             flash(f"The password is wrong or does not match e-mail: {form.email.data}."
                   + "Please recheck your password and e-mail", category="error")
@@ -85,8 +91,47 @@ def signout():
     return redirect(url_for("home"))
 
 
-@app.route("/account")
+def size_down_image(form_image):
+    """Size down images using pillow."""
+    output_size = (125, 125)
+    new_image = Image.open(form_image)
+    new_image.thumbnail(output_size)
+    return new_image
+
+
+def save_image(form_image):
+    """Save image."""
+    random_hex = secrets.token_hex(8)
+    _, file_extension = os.path.splitext(form_image.filename)
+    image_file_name = random_hex + file_extension
+    image_path = os.path.join(app.root_path, "static/profile_pics", image_file_name)
+    sized_down_image = size_down_image(form_image)
+    sized_down_image.save(image_path)
+    return image_file_name
+
+
+@app.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
     """Show current user account details."""
-    return render_template("account.html", title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        same_username = current_user.username == form.username.data
+        same_email = current_user.email == form.email.data
+        if same_username and same_email and not form.image.data:
+            flash("There are no changes to be commited. Nothing changed",
+                  category="info")
+            return redirect(url_for("account"))
+        if form.image.data:
+            image_file = save_image(form.image.data)
+            current_user.image_file = image_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated", category="success")
+        return redirect(url_for("account"))
+    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
+    form.username.data = current_user.username
+    form.email.data = current_user.email
+    return render_template("account.html",
+                           title="Account", image_file=image_file, form=form)
